59ef644b8c8f6912ed0401c05b89b722f22767cf
zaire <zairecrypto@gmail.com>
2020-03-03 19:49:56 +0200

    first commit

--- lib/my_tests_web/controllers/page_controller.ex
+++ lib/my_tests_web/controllers/page_controller.ex
@@ -2,8 +2,7 @@ defmodule MyTestsWeb.PageController do
   use MyTestsWeb, :controller
 
   def index(conn, _params) do
-    file = "image/phoenix.png"
+    file = "static/image/phoenix.png"
     render conn, "index.html", file: file
   end
 end
-                             
